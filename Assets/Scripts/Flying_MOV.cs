309f4408ab8cd8d7972d94bae62022949a102bca
Nailfighter <nailfighter000@gmail.com>
2020-06-07 17:05:18 +0530

    Added(Rocket building and Input Movement )

---
+++ Assets/Scripts/Flying_MOV.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class Flying_MOV : MonoBehaviour
+{
+    public Rigidbody roc_rg;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        roc_rg = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        Rocket_input();
+        
+    }
+
+    private void Rocket_input()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            roc_rg.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
+
