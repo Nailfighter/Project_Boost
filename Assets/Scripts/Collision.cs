f9504dcdbbd6646aec736f48eaab23a2ee885110
Nailfighter <nailfighter000@gmail.com>
2020-06-08 17:49:13 +0530

    Added partictle effect and fixed audio

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -1,14 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
 public class Collision : MonoBehaviour
 {
+    [SerializeField] int waittime=1;
+    [Header("Audio")]
+    public AudioSource audioSource;
+    public AudioClip boom;
+    public AudioClip finish;
     private void OnCollisionEnter(UnityEngine.Collision collision)
     {
-        if (GetComponent<Flying_MOV>().state==Flying_MOV.Game_state.collided)
+        if (GetComponent<Flying_MOV>().state == Flying_MOV.Game_state.collided)
         {
             return;
         }
@@ -17,16 +20,32 @@ public class Collision : MonoBehaviour
             case "Hell":
                 Debug.Log("Hell");
                 GetComponent<Flying_MOV>().state = Flying_MOV.Game_state.collided;
-                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                audioSource.Stop();
+                audioSource.PlayOneShot(boom);
+                Invoke("Return_Scene", waittime);
                 break;
             case "Pad":
                 Debug.Log("Safe");
-                GetComponent<Flying_MOV>().state = Flying_MOV.Game_state.Playing;
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                GetComponent<Flying_MOV>().state = Flying_MOV.Game_state.collided;
+                print("playing");
+                audioSource.Stop();
+                audioSource.PlayOneShot(finish);
+                Invoke("Next_Scene", waittime);
                 break;
         }
-   
+
     }
+
+    public void Next_Scene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Return_Scene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
     private void Update()
     {
         if (Input.GetKey(KeyCode.R))
--- Assets/Scripts/Flying_MOV.cs
+++ Assets/Scripts/Flying_MOV.cs
@@ -5,32 +5,44 @@ using UnityEngine;
 using UnityEngine.Animations;
 using UnityEngine.SceneManagement;
 
+
 public class Flying_MOV : MonoBehaviour
 {
     [SerializeField] Rigidbody roc_rg;
-    [SerializeField] AudioSource audioSource;
-    [Range(100f,200f)]
+    [SerializeField] int waittime = 1;
+    [Range(100f, 200f)]
     [SerializeField] float side_thrust = 100f;
     [Range(10f, 100f)]
     [SerializeField] float main_thrust = 100f;
-    public enum Game_state { Main_Menu,Playing,Finish,Pause,collided}
+    [Header("Audio")]
+    public AudioClip boom;
+    public AudioClip finish;
+    public AudioClip thrust;
+    public AudioSource Thrust_audiosource;
+    public AudioSource Impact_audiosource;
+    [Header("Particle System")]
+    [SerializeField] ParticleSystem engine_thrust;
+    [SerializeField] ParticleSystem expoltion;
+    [SerializeField] ParticleSystem confetti;
+
+    public enum Game_state { Main_Menu, Playing, Finish, Pause, collided }
     public Game_state state = Game_state.Main_Menu;
-    public int Offset; 
 
     // Update is called once per frame
     private void Start()
     {
         roc_rg = GetComponent<Rigidbody>();
+
     }
     void Update()
     {
         Rotation();
         Thrust();
+        Poss_Restric();
         if (Input.GetKey(KeyCode.R))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
-        Poss_Restric();
 
     }
 
@@ -46,7 +58,7 @@ public class Flying_MOV : MonoBehaviour
         roc_rg.freezeRotation = true;
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Rotate(Vector3.forward*side_thrust*Time.deltaTime);
+            transform.Rotate(Vector3.forward * side_thrust * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
@@ -60,16 +72,56 @@ public class Flying_MOV : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             roc_rg.AddRelativeForce(Vector3.up * main_thrust);
-            if (!audioSource.isPlaying)
+            engine_thrust.Play();
+            if (!Thrust_audiosource.isPlaying)
             {
-                audioSource.Play();
+                Thrust_audiosource.PlayOneShot(thrust);
             }
+            
+            
         }
-        else
+        else 
         {
-            audioSource.Stop();
+            Thrust_audiosource.Stop();
+            print("rrrr");
+            engine_thrust.Stop();
         }
+
+
+    }
+    public void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        if (Impact_audiosource.isPlaying) { return; }
+        switch (collision.collider.tag)
+        {
+            case "Hell":
+                Death_Sequence();
+                break;
+            case "Pad":
+                Debug.Log("Safe");
+                Fin_Sequence();
+                break;
+        }
+        
+
     }
 
+    private void Death_Sequence()
+    {
+        Debug.Log("Hell");
+        GetComponent<Flying_MOV>().state = Flying_MOV.Game_state.collided;
+        expoltion.Play();
+        Impact_audiosource.Stop();
+        Impact_audiosource.PlayOneShot(boom);
+    }
+
+    private void Fin_Sequence()
+    {
+        GetComponent<Flying_MOV>().state = Flying_MOV.Game_state.collided;
+        print("playing");
+        confetti.Play();
+        Impact_audiosource.Stop();
+        Impact_audiosource.PlayOneShot(finish);
+    }
 }
 
